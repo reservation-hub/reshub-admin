cf12ebc3374ac199f861342bf120355c28726028
Jang Yoonsung <upthe15752@gmail.com>
2021-09-07 11:45:39 +0900

    dashboard修正

--- app/src/store/types/dashboardTypes.ts
+++ app/src/store/types/dashboardTypes.ts
@@ -1,25 +1,13 @@
-import { Area, City, Prefecture } from '../../entities/Location'
 import { User } from '../../entities/User'
+import { ShopList } from '../../Model/ShopResponse'
 
 export const DASHBOARD_REQUEST_START = 'DASHBOARD_REQUEST_START' as const
 
 export const DASHBOARD_REQUEST_SUCCESS = 'DASHBOARD_REQUEST_SUCCESS' as const
 
-export type DashboardShop = {
-  name: string
-  address: string
-  area: Area[]
-  city: City[]
-  prefecture: Prefecture[]
-  id: number
-  reservationsCount: number
-  stylistsCount: number
-  phoneNumber: number
-}
-
 export type DashBoardResponseType = {
   user: { users: User[], totalCount: number }
-  shop: { shopData: DashboardShop[], totalCount: number }
+  shop: { shopData: ShopList[], totalCount: number }
 }
 
 export type DashBoardState = {
