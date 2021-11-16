61e4c7e9bef4889da544dd4d4794fbcb07efc005
Jang Yoonsung <upthe15752@gmail.com>
2021-11-16 16:04:13 +0900

    materila2tailwind

---
+++ app/tailwind.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/**/*.tsx', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // color増えるかも？
+      colors: {
+        primary: {
+          DEFAULT: '#F2BDCD',
+          light: 'rgba(242, 189, 205, 0.5)',
+          dark: '#011627'
+        },
+        secondary: {
+          main: '#FDFFFC',
+          light: '#FEFEFE',
+          dark: '#E5E5E5'
+        },
+        error: {
+          main: '#F44336',
+          light: 'rgba(244, 67, 54, 0.5)',
+          dark: '#E31B0C'
+        },
+        success: {
+          main: '#4CAF50',
+          light: 'rgba(76, 175, 79, 0.5)',
+          dark: '#459648'
+        }
+      },
+      flex: { ...Array.from(Array(12 + 1)).map((_, i) => `${i} ${i} 0%`) }, // ex) flex-1(flex: 1 1 0%)
+      fontFamily: {},
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      },
+      background: {
+        'main-img': 'url(https://images.unsplash.com/photo-1522337360788-8b13dee7a37e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80)'
+      }
+    }
+  },
+  variants: {
+
+  },
+  plugins: []
+}
