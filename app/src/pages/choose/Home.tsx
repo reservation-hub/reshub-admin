1aaf89650ffdeab89a62775de9e8fb35f7e20157
Jang Yoonsung <upthe15752@gmail.com>
2021-11-30 18:14:54 +0900

    global style追加・page名fix

--- app/src/pages/choose/Home.tsx
+++ app/src/pages/choose/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import LoginSelectHeader from '@components/common/loginSelect/LoginSelectHeader'
 import CommonStyle from '@components/common/CommonStyle'
-import PageSelect from '@components/home/PageSelect'
+import PageSelect from '@/components/choose/PageSelect'
 import LoginSelectFooter from '@components/common/loginSelect/LoginSelectFooter'
 
 function Home() {
