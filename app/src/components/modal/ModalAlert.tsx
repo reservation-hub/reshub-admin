a1db4ea7549f417893067ab30abad40993efdc39
Jang Yoonsung <upthe15752@gmail.com>
2021-12-07 20:54:28 +0900

    fmt

--- app/src/components/modal/ModalAlert.tsx
+++ app/src/components/modal/ModalAlert.tsx
@@ -7,15 +7,14 @@ const ModalAlert = ({
   alertText,
   modalCloseHandler,
   modalHandler,
-  buttonText,
+  buttonText
 }: IModalProps) => {
-
   return (
     <div className='w-[30rem] bg-secondary-main p-[3rem] text-center rounded-[.5rem]'>
       <span className='text-[1.6rem]'>{alertText}</span>
       <div className='flex justify-between mt-4'>
         <CustomButton onClick={modalHandler}>{buttonText}</CustomButton>
-        <CustomButton onClick={() => (modalCloseHandler)}>戻る</CustomButton>
+        <CustomButton onClick={() => modalCloseHandler}>戻る</CustomButton>
       </div>
     </div>
   )
