8878979835441cea35b56b9d5b81cae25ad991cf
Jang Yoonsung <upthe15752@gmail.com>
2021-12-11 17:17:46 +0900

    column追加

--- app/src/components/dashboards/Shopdashboard.tsx
+++ app/src/components/dashboards/Shopdashboard.tsx
@@ -2,7 +2,7 @@ import SubHeader from '@/components/common/atoms/SubHeader'
 import TableLayout from '@/components/common/atoms/TableLayout'
 import Section from '@/components/common/layout/Section'
 import { ClassesAndChildren } from '@/components/common/_PropsType'
-import { SALON_CELL, STYLELIST_CELL } from '@/constants/Table'
+import { RESERVATION_CELL, SALON_CELL, STYLELIST_CELL } from '@/constants/Table'
 import { salonIndexShopStaffResponse } from '@/utils/api/request-response-types/Dashboard'
 import React from 'react'
 import SalonItem from '@components/list/Shop/SalonItem'
@@ -16,7 +16,7 @@ const ShopDashboard = ({ data }: ShopStaffDashboardProps) => {
     <Section>
       <SubHeader title='ダッシュボード' type='dashboard' />
       <div className='flex justify-between'>
-        <TableLayout>ここ予約</TableLayout>
+        <TableLayout cell={RESERVATION_CELL}>ここ予約</TableLayout>
         <TableLayout cell={STYLELIST_CELL}>ここstylist</TableLayout>
       </div>
       <TableLayout cell={SALON_CELL}>
