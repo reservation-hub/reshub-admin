4d479f2ddafae9337689ace77b011ec37799d916
Jang Yoonsung <upthe15752@gmail.com>
2021-12-18 09:33:58 +0900

    fix

--- app/src/components/form/SalonForm.tsx
+++ app/src/components/form/SalonForm.tsx
@@ -45,8 +45,6 @@ const SalonForm = ({
     days: Days.map((day) => ({ value: String(day.id), name: day.name }))
   }
 
-  console.log(data)
-
   if (
     formValue.name.length === 0 ||
     formValue.address.length === 0 ||
