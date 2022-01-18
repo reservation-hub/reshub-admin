908e33ff11deecbac73410104e0f09f79e53bf24
Eugene Sinamban <eugene.sinamban@gmail.com>
2022-01-19 00:15:05 +0900

    add dayjs plugin

--- app/src/utils/hooks/useConvertScheduleTimeToDateString.ts
+++ app/src/utils/hooks/useConvertScheduleTimeToDateString.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs'
+import dayjs from '@utils/hooks/useDayJs'
 import { currentDate } from '@constants/Time'
 const convertScheduleTimeToDateString = (dateString: string): string =>
   dayjs(`${currentDate} ${dateString}:00`).format('YYYY-MM-DDTHH:mm:ss')
